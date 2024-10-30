a9a6b5086e4566d6fd9d041b9af1a4180479e6c4
Tero <etehe004@edu.xamk.fi>
2024-10-30 14:40:00 +0200

    Logitus siirretty terminalista Logit kansioon

--- SkriptV2/scriptKAIKKIVERSIO.py
+++ SkriptV2/scriptKAIKKIVERSIO.py
@@ -2,30 +2,47 @@ import csv
 import subprocess
 import time
 import threading
+import os
 
 
 #Funktio threadingia varten
 #Käytännössä tarkotus on että voi kutsua helposti 
 def scriptinloppu(ip_address, nimi):
-	subprocess.run(['expect', 'sshtarkistus.exp', ip_address], check=True)
+	sshlog = f"Logit/SSH_{ip_address}.txt"
+	with open(sshlog, "w") as tiedosto:
+		subprocess.run(['expect', 'sshtarkistus.exp', ip_address], check=True, stdout=tiedosto, stderr=tiedosto)
 	print(f"{nimi} ssh tarkistus suoritettu osoitteella {ip_address} ")
-	subprocess.run(['expect', 'konfigurointi.exp', ip_address], check=True)
+	konflog = f"Logit/CONF_{ip_address}.txt"
+	with open(konflog, "w") as tiedosto2:
+		subprocess.run(['expect', 'konfigurointi.exp', ip_address], check=True, stdout=tiedosto2, stderr=tiedosto2)
 	print(f"{nimi} konfigurointi suoritettu osoitteella {ip_address}")
 	time.sleep(20)
-	subprocess.run(['expect', 'sshtarkistus.exp', ip_address], check=True)
+	with open(sshlog, "w") as tiedosto:
+		subprocess.run(['expect', 'sshtarkistus.exp', ip_address], check=True, stdout=tiedosto, stderr=tiedosto)
 	print(f"{nimi} ssh tarkistus suoritettu osoitteella {ip_address} ")
-	subprocess.run(['expect', 'clusteri.exp', ip_address], check=True)
+	clustlog = f"Logit/CLUST_{ip_address}.txt"
+	with open(clustlog, "w") as tiedosto3:
+		subprocess.run(['expect', 'clusteri.exp', ip_address], check=True, stdout=tiedosto3, stderr=tiedosto3)
 	print(f"{nimi} Clusteri.exp suoritettu osoitteella {ip_address}")
 	print("------------------------------------------------------------")
 	for row in rows:
 		if row['Nimi'] == nimi:
 			row['Konfiguroitu'] = 'True'
-	
+			with open(output_file, mode='w', newline='') as file:  # Tää on vähän huono ratkau mutta parempaa hakies toimii
+				fieldnames = ['Nimi', 'MAC-osoite', 'Konfiguroitu']
+				writer = csv.DictWriter(file, fieldnames=fieldnames)
+				writer.writeheader() 
+				writer.writerows(rows)  
+				print(rows) # Debug printti
 
 
 input_file = 'Tiedot.csv'
 output_file = 'Tiedot2.csv'
 dhcp_conf_file = '/etc/dhcp/dhcpd.conf'
+
+		
+		
+
 
 static_config = """subnet 192.168.1.0 netmask 255.255.255.0 {
   range 192.168.1.100 192.168.1.150;
@@ -63,7 +80,7 @@ with open(input_file, mode='r', newline='') as file:
         rows.append(row)
 
 
-print(f"CSV-tiedosto '{output_file}' on päivitetty.")
+
 
 with open(dhcp_conf_file, mode='w') as file:
     file.write(static_config)
@@ -90,10 +107,4 @@ for row in rows:
 #Odotetaan että threadit on valmiita
 for thread in threads:
 	thread.join()
-
-with open(output_file, mode='w', newline='') as file:
-    fieldnames = ['Nimi', 'MAC-osoite', 'Konfiguroitu']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(rows)
-    print(rows) #debug printti
+	

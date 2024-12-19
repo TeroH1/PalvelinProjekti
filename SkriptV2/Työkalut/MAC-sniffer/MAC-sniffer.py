9d5da3701c019752a4264990e5288e83654aa359
TeroH1 <tero-h@hotmail.com>
2024-12-19 12:53:53 +0200

    Paranneltu versio MAC snifferistä. Tarkastaa onko MAC osoite jo listalla eikä lisää jos on.

--- SkriptV2/Työkalut/MAC-sniffer/MAC-sniffer.py
+++ SkriptV2/Työkalut/MAC-sniffer/MAC-sniffer.py
@@ -4,12 +4,15 @@
 from scapy.all import *
 import csv
 import os
+interface = "ens33"
+#Muuttuja joka seuraa tällä kerralla tulleita MAC osotteita
+sessioMACosoitteet = {}
 
 #Tehdään tiedosto jos sitä ei ole
 if not os.path.exists("mac-osoitteet.csv"):
 	with open('mac-osoitteet.csv', mode='w', newline='') as file:
 		writer = csv.writer(file)
-		writer.writerow(["Nimi", "MAC-osoite","Loopback","Link-ip/subnet", "Link-naapuri"])  
+		writer.writerow(["Nimi", "MAC-osoite","Loopback","Link-ip/subnet", "Link-naapuri", "Konfiguroitu"])  
 		
 #laitetaan CSV-tiedostosta tiedot sanakirjaan niin saadaan käytyä läpi jos tulee samoja
 tallennettavat_tiedot = {}	
@@ -21,6 +24,7 @@ with open("mac-osoitteet.csv", mode='r', newline='') as file:
 		loopback = row['Loopback']
 		linkip = row['Link-ip/subnet']
 		linknaapuri = row['Link-naapuri']
+		konfiguroitu = row['Konfiguroitu']
 		tallennettavat_tiedot[laite] = mac_osoite  
 
 
@@ -30,7 +34,6 @@ writer = csv.writer(file)
 
 
 def discover_snifferi(packet):
-
 	if packet.haslayer(DHCP):
 		for opt in packet[DHCP].options:
 			#Etsitään DHCP Discover -viestejä
@@ -38,11 +41,19 @@ def discover_snifferi(packet):
 				if packet[Ether].src.startswith(vendor):
 					print(f"DHCP discover macistä: {packet[Ether].src}")
 					macosoite = packet[Ether].src
-					if macosoite in tallennettavat_tiedot.values():
-						print("\nMAC-osoite on jo listassa TESTAUSMOODISSA ELI LAITETAAN SILTI LISTAAN!")
-						print("Ota # merkki pois tässä allaolevalta riviltä jos haluat ettei samoja maceja laiteta listaan")
-						#break
+					if macosoite in tallennettavat_tiedot.values() or macosoite in sessioMACosoitteet.values():
+						print(f"\nMAC-osoite on jo listassa nimellä:\n")
+						for avain, arvo in tallennettavat_tiedot.items():
+							if arvo == macosoite:
+								print(f"{avain}	--- Aiemmalla sessiolla lisätty")
+						
+						for avain, arvo in sessioMACosoitteet.items():
+							if arvo == macosoite:
+								print(f"{avain}	--- Tällä sessiolla lisätty")
+							
+						break
 					laite = input("Anna nimi laitteelle(PVExx): ")
+					sessioMACosoitteet[laite] = macosoite 
 					tallennettavat_tiedot.update({laite:macosoite})
 					loopback = input(f"{laite} - anna loopback osoite BGP yhteyttä varten (muodossa 10.2.1.48)=: ")
 					tallennettavat_tiedot.update({laite:loopback})
@@ -54,15 +65,17 @@ def discover_snifferi(packet):
 					
 					#Huom. print
 					print("\nHUOM!!!!!!!!!\nLinknaapurista käytetään vain loppuosaa, koska redunttanttinen yhteys eli myös 2.96 käytetään")
-					linknaapuri = input(f" - {laite} - anna BGP leaf naapurin ip osoite (muodossa 10.10.1.96): ")
+					linknaapuri = input(f" - {laite} - anna BGP leaf naapurin ip osoite (muodossa 10.10.1.96):\n ")
 					tallennettavat_tiedot.update({laite:linknaapuri})
+					konfiguroitu = "False"
+					tallennettavat_tiedot.update({laite:konfiguroitu})
 					#laitetaan tiedot csv-tiedostoon
-					writer.writerow([laite, macosoite, loopback, linkip, linknaapuri])
+					writer.writerow([laite, macosoite, loopback, linkip, linknaapuri,konfiguroitu])
 		        
 					break
 
 #Katotaan DHCP paketit porttien perusteella
 vendor = input("Anna MAC-osoitteen vendor osa/muutama numero alusta (tyhjä = kaikki)(dell 14:18:77)")
-sniff(iface="ens33", filter="udp and (port 67 or port 68)", prn=discover_snifferi, store=0)
+sniff(iface=interface, filter="udp and (port 67 or port 68)", prn=discover_snifferi, store=0)
 
 file.close()

51c8b7b89bafa5c9a62a89d4d3d4c52fd6dbf5cd
OlliK <olli.a.kortelainen@gmail.com>
2025-01-08 21:46:05 +0200

    apt-cacher mukaan

--- SkriptV2/Työkalut/Testitoiminnot/testi.sh
+++ SkriptV2/Työkalut/Testitoiminnot/testi.sh
@@ -2,4 +2,4 @@
 sudo systemctl status apache2 --no-pager
 sudo systemctl status isc-dhcp-server --no-pager
 sudo systemctl status tftpd-hpa.service --no-pager
-
+sudo systemctl status apt-cacher-ng.service --no-pager

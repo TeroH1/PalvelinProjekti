5f2d1d199efdcf1dabd1dfd4f7efb92c9454a573
OlliK <olli.a.kortelainen@gmail.com>
2025-01-08 23:01:17 +0200

    Update ASENNA_KAIKKI.sh

--- SkriptV2/Työkalut/ASENNA_KAIKKI.sh
+++ SkriptV2/Työkalut/ASENNA_KAIKKI.sh
@@ -47,7 +47,7 @@ chmod 777 /var/www/html/PVEXXX/answer.toml
 apt install gh
 apt install expect
 apt install apt-cacher-ng
-systemctl start apt-cacher-ng
+sudo systemctl start apt-cacher-ng
 sudo sh -c 'echo "Acquire::http::Proxy \"http://192.168.1.1:3142\";" > /etc/apt/apt.conf.d/01proxy'
 cp /etc/apt/sources.list /var/www/html/sourceslist.txt
 cp /etc/apt/apt.conf.d/01proxy /var/www/html/01proxy.txt
